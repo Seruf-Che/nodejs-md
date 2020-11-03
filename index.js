b1478899473d9a2b39df02249a692a74cdcc2c32
Seruf-Che <33310590+Seruf-Che@users.noreply.github.com>
2020-11-03 16:52:31 +0200

    MongoDB conection
    dotenv package installed
    environment variables added
    MongoDB conected

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const PORT = 3012;
 
+require('dotenv').config();
 const app = express();
 
-app.listen(3012, () => console.log('Server is started'));
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@nodejs-md.axtof.mongodb.net/${process.env.MONGO_DB_DBNAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+.then(() => console.log("Connected to database"))
+.catch((err) => console.log("Database connection was failed", err));
+
+app.listen(PORT, () => console.log(`Server is running port ${PORT}`));
