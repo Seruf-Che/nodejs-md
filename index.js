2e65ff0f4f8eb1e60922ddf2ed3cf8cc3a24b2aa
Seruf-Che <33310590+Seruf-Che@users.noreply.github.com>
2020-11-04 11:38:18 +0200

    Routes added, refactoring
    registrationRoute added
    loginRoute added
    citiesRoute added

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const registrationRoute = require('./src/routes/registration');
+const loginRoute = require('./src/routes/login');
+const citiesRoute = require('./src/routes/cities');
+
 const PORT = 3012;
 
-require('dotenv').config();
 const app = express();
 
 mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@nodejs-md.axtof.mongodb.net/${process.env.MONGO_DB_DBNAME}?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@nodejs-md.axtof.mongodb.net/${process.env.MONGO_DB_DBNAME}?w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -14,5 +19,9 @@ mongoose.connect(
 )
 .then(() => console.log("Connected to database"))
 .catch((err) => console.log("Database connection was failed", err));
+
+app.use(registrationRoute);
+app.use(loginRoute);
+app.use(citiesRoute);
 
 app.listen(PORT, () => console.log(`Server is running port ${PORT}`));
