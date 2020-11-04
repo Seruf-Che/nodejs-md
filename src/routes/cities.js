b6a29984cd210c81a5664888a12726976719f992
Seruf-Che <33310590+Seruf-Che@users.noreply.github.com>
2020-11-04 11:36:28 +0200

    Cities route added

---
+++ src/routes/cities.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+router
+  .route("/cities")
+  .get((req, res) => {
+    const query = req.query.query;
+    res.send(`City query: ${query}`);
+  })
+  .post((req, res) => {
+    res.send("City added");
+  });
+
+router
+  .route("/cities/:id")
+  .get((req, res) => {
+    const id = req.params.id;
+    res.send(`City id ${id}`);
+  })
+  .put((req, res) => {
+    const id = req.params.id;
+    res.send(`City ${id} updated`);
+  })
+  .delete((req, res) => {
+    const id = req.params.id;
+    res.send(`City ${id} deleted`);
+  });
+
+
+module.exports = router;
