65e9ea2b84877c77f705e1a1a08a93ce61b50635
Seruf-Che <33310590+Seruf-Che@users.noreply.github.com>
2020-11-04 12:15:18 +0200

    bodyParser added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const registrationRoute = require('./src/routes/registration');
@@ -9,6 +10,8 @@ const citiesRoute = require('./src/routes/cities');
 const PORT = 3012;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 mongoose.connect(
   `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@nodejs-md.axtof.mongodb.net/${process.env.MONGO_DB_DBNAME}?w=majority`,
@@ -17,8 +20,8 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 )
-.then(() => console.log("Connected to database"))
-.catch((err) => console.log("Database connection was failed", err));
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log("Database connection was failed", err));
 
 app.use(registrationRoute);
 app.use(loginRoute);
